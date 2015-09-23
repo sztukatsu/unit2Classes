a6513b8501647d74e9d0e282c2e2c53a20cd0033
Kevin Tu <kntu@NN121-HA-21.HS.sd203.org>
2015-09-23 10:29:26 -0500

    hi

---
+++ target car/Chap3Ex15.java
@@ -0,0 +1,29 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.swing.JComponent;
+import java.awt.geom.Ellipse;
+
+
+public class Chap3Ex15
+ {
+        
+ public class Target
+ {
+     private int xLeft;
+     private int yTop;
+ }
+ 
+ public Target(int x, int y)
+ {
+     xLeft = x;
+     yTop = y;
+ }
+       
+ public void draw(Grphics2D g2)
+ {
+     Ellipse2D.Double circle5
+        = new Ellipse2D.Double(
+ }
+   
+
+}
