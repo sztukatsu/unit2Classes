a0fcc04cda7ddd86a400861885f156e0b121ab56
Kevin Tu <kntu@NN121-HA-21.HS.sd203.org>
2015-09-23 10:14:47 -0500

    hi

---
+++ AvgOfThree/door.java
@@ -0,0 +1,27 @@
+
+
+/**
+ * Write a description of class door here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class door
+{
+   private boolean isLocked;
+   private boolean isOpened;
+   
+   public Door()
+   {
+       this.isLocked = false;
+       this.isOpened = false;
+    }
+    
+   public Door(boolean locked, boolean opened)
+   {
+       this.isLocked = locked;
+       this.isOpened = opened;
+    }
+    
+   public void lock()
+}
---
+++ Chapter 3 Practice/Car.java
@@ -0,0 +1,41 @@
+
+
+/**
+
+ */
+public class Car
+{
+   /** fuel efficiency of the car in mpg    **/
+   private double fuelEfficiency;
+   
+   /** amount of fuel left in the car's tank in gallons  */
+   private double fuelInTank;
+    
+    
+  
+    public Car(double fuelEfficiency)
+    {
+        fuelEfficiency = fuelEfficiency;
+         fuelInTank = 0;
+        }
+
+    public void drive( double distance)
+    {
+        fuelInTank -= distance/fuelEfficiency;
+        
+    }
+
+   
+    public double getGasInTank()
+    {
+         return fuelInTank;
+    }
+
+  
+    public void addGas(double gallonsOfGas)
+    {
+        fuelInTank += gallonsOfGas;
+
+    }
+
+}
